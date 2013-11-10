4812da0b5ba247ffc413e36ac026677022b6ef7e
Chris Maloney <voldrani@gmail.com>
2013-11-10 15:21:43 -0500

    Cute maze generator

--- zombo.js
+++ zombo.js
@@ -2,12 +2,14 @@
     // Canvas context
     var ctx;
 
+    // Math flashcards
     var math_problem = function() {
         var problem = (Math.floor((Math.random() * 6))) + " + " +
                       (Math.floor((Math.random() * 6)));
         console.info(problem);
         $('#question').text(problem);
     };
+
 
 
     function init_canvas() {
@@ -17,12 +19,235 @@
         }
     }
 
-    function make_maze() {
-            ctx.fillStyle = "rgb(200, 0, 0)";
-            ctx.fillRect (0, 0, 1000, 500);
+    var maze = {
+        num_rows: 5,
+        num_cols: 5,
+        cell_width: 100,
+        cell_height: 100,
+        cells: [],   // 2d array of objects
+        walls: []
+    };
 
-            ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-            ctx.fillRect (30, 30, 55, 50);
+    function draw_maze() {
+        var walls = maze.walls;
+        var num_walls = walls.length;
+        for (var wn = 0; wn < num_walls; ++wn) {
+            var w = walls[wn];
+            if (w.exists) {
+                draw_wall(w);
+            }
+        }
+    }
+    function draw_wall(w) {
+        ctx.fillStyle = "rgb(200, 0, 0)";
+        put_wall(w);
+    }
+    function erase_wall(w) {
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        put_wall(w);
+    }
+    function put_wall(w) {
+        //console.info("wall " + wn);
+        var left = maze.cell_width * w.col;
+        var top = maze.cell_height * w.row;
+        var width  = w.orientation == "horizontal" ? maze.cell_width : 5;
+        var height = w.orientation == "horizontal" ? 5 : maze.cell_height;
+        ctx.fillRect(left, top, width, height);
+    }
+
+
+    function make_maze() {
+        var num_rows = maze.num_rows;
+        var num_cols = maze.num_cols;
+        var num_cells = num_rows * num_cols;
+        var cells = maze.cells;
+        var walls = maze.walls;
+
+
+        // First initialize the two-dimensional array of the cell objects
+        for (var r = 0; r < num_rows; ++r) {
+            var cell_row = cells[r] = [];
+            for (var c = 0; c < num_cols; ++c) {
+                var cell = cell_row[c] = {
+                    visited: false,
+                    walls: {}
+                };
+            }
+        }
+
+        // Now initialize the wall objects, such that all the walls
+        // have "exists: 1".
+        for (var r = 0; r < num_rows; ++r) {
+            var cell_row = cells[r];
+            for (var c = 0; c < num_cols; ++c) {
+                var cell = cell_row[c];
+
+                // Add cell walls, taking into account that cells share walls
+                var cw = cell.walls;
+
+                // north: if there is a cell to the north, that already has
+                // a southern wall, then use that.
+                if (r > 0 && cells[r-1][c].walls.S) {
+                    cw.N = cells[r-1][c].walls.S;
+                }
+                else {
+                    cw.N = {
+                        exists: true,
+                        orientation: 'horizontal',
+                        row: r,
+                        col: c
+                    };
+                    walls.push(cw.N);
+                }
+
+                // east
+                if (c < num_cols - 1 && cells[r][c+1].walls.W) {
+                    cw.E = cells[r][c+1].walls.W;
+                }
+                else {
+                    cw.E = {
+                        exists: true,
+                        orientation: 'vertical',
+                        row: r,
+                        col: c+1
+                    };
+                    walls.push(cw.E);
+                }
+
+                // south
+                if (r < num_rows - 1 && cells[r+1][c].walls.N) {
+                    cw.S = cells[r+1][c].walls.N;
+                }
+                else {
+                    cw.S = {
+                        exists: true,
+                        orientation: 'horizontal',
+                        row: r+1,
+                        col: c
+                    };
+                    walls.push(cw.S);
+                }
+
+                // west
+                if (c > 0 && cells[r][c-1].walls.E) {
+                    cw.W = cells[r][c-1].walls.E;
+                }
+                else {
+                    cw.W = {
+                        exists: true,
+                        orientation: 'vertical',
+                        row: r,
+                        col: c
+                    };
+                    walls.push(cw.W);
+                }
+            }
+        }
+        // If you want to see it work while debugging, uncomment:
+        //draw_maze();
+
+        // Set the mole to a random place to start, and initialize start
+        // and finish flags
+        var got_start = false;
+        var got_finish = false;
+        var mole = {};
+        var num_cells_visited = 0;
+
+        // Move the mole into a specific cell
+        function move_mole(row, col) {
+            mole.r = row;
+            mole.c = col;
+            cells[row][col].visited = true;
+            num_cells_visited++;
+        }
+
+        // Start in a random cell
+        move_mole(Math.floor(Math.random() * num_rows),
+                  Math.floor(Math.random() * num_cols));
+
+        // When we hit a dead-end, "hop" the mole to a cell that we've
+        // already visited.
+        function hop_mole() {
+            do {
+                mole.r = Math.floor(Math.random() * num_rows);
+                mole.c = Math.floor(Math.random() * num_cols);
+            }
+            while (!cells[mole.r][mole.c].visited);
+        }
+
+        // This says when we're done
+        function done_digging() {
+            return got_start && got_finish && num_cells_visited == num_cells;
+        }
+
+        // Start digging
+        while (!done_digging()) {
+            var r = mole.r;
+            var c = mole.c;
+            var cell = cells[r][c];
+
+            // which directions can we go from here?
+            var good_directions = [];
+            // North?
+            if ((r == 0 && !got_start) || (r > 0 && !cells[r-1][c].visited)) {
+                good_directions.push('N');
+            }
+            // East?
+            if (c < num_cols - 1 && !cells[r][c+1].visited) {
+                good_directions.push('E');
+            }
+            // South?
+            if ((r == num_rows-1 && !got_finish) ||
+                (r < num_rows-1 && !cells[r+1][c].visited)) {
+                good_directions.push('S');
+            }
+            // West?
+            if (c > 0 && !cells[r][c-1].visited) {
+                good_directions.push('W');
+            }
+
+            // Can we go anywhere?
+            var num_dirs = good_directions.length;
+            if (num_dirs == 0) {
+                // No, we need to hop the mole
+                hop_mole(mole);
+            }
+            else {
+                // Pick one of those directions, and dig there
+                var dir = good_directions[ Math.floor(Math.random() * num_dirs) ];
+                // Clear the wall
+                cell.walls[dir].exists = false;
+                // If you want to see it work while debugging, uncomment:
+                //erase_wall(cell.walls[dir]);
+                // Move the mole
+                if (dir == 'N') {
+                    if (r == 0) {
+                        got_start = true;
+                        if (!done_digging()) hop_mole();
+                    }
+                    else {
+                        move_mole(r-1, c);
+                    }
+                }
+                else if (dir == 'E') {
+                    move_mole(r, c+1);
+                }
+                else if (dir == 'S') {
+                    if (r == num_rows - 1) {
+                        got_finish = true;
+                        if (!done_digging()) hop_mole();
+                    }
+                    else {
+                        move_mole(r+1, c);
+                    }
+                }
+                else if (dir = 'W') {
+                    move_mole(r, c-1);
+                }
+            }
+        }
+
+        draw_maze();
     }
 
     // document ready function
